8ed37105ac4dc958d9eb6cb78543bbfcdc324d43
Wooyoung Myung <mwy3055@gmail.com>
2021-11-20 15:02:01 +0900

    To-Do List - Keep in memory in To-Do List (4 / 4)

--- frontend/To-Do List/To-Do List/task/src/script.js
+++ frontend/To-Do List/To-Do List/task/src/script.js
@@ -1,9 +1,27 @@
-const taskList = document.getElementById("task-list");
+const taskList = document.getElementById("task-list"); // HTML element (unordered list)
+const parsed = JSON.parse(localStorage.getItem("tasks"));
+let tasks = parsed || [];
+let taskCount = tasks.length;
+
+console.log("Loaded: " + tasks.length);
+
+for (const taskIndex in tasks) {
+    let taskName = tasks[parseInt(taskIndex)].task;
+    console.log("Task: " + taskName);
+    addTask(taskName);
+}
 
 document.getElementById("add-task-button").addEventListener("click", function () {
     const inputTask = document.getElementById("input-task");
     const input = inputTask.value;
-    if (input !== "") {
+    addTask(input);
+    tasks.push(createTask(input));
+    storeTasks(tasks);
+    inputTask.value = "";
+});
+
+function addTask(taskName) {
+    if (taskName !== "") {
         const task = document.createElement("li");
         
         const label = document.createElement("label");
@@ -18,7 +36,7 @@ document.getElementById("add-task-button").addEventListener("click", function ()
         
         const span = document.createElement("span");
         span.className = "task";
-        span.innerHTML = input;
+        span.innerHTML = taskName;
         task.appendChild(span);
         
         const deleteButton = document.createElement("button");
@@ -27,43 +45,56 @@ document.getElementById("add-task-button").addEventListener("click", function ()
             onDeleteButtonClick(deleteButton);
         });
         const i = document.createElement("i");
-        i.classList.add("fa", "fa-close");
-        i.innerHTML = "Delete";
+        i.classList.add("fa", "fa-close")
         deleteButton.appendChild(i);
         task.appendChild(deleteButton);
         
         taskList.appendChild(task);
     }
-    inputTask.value = "";
-});
-
-const deleteButtons = taskList.getElementsByClassName("delete-btn");
-for (const button in deleteButtons) {
-    const deleteButton = deleteButtons[parseInt(button)];
-    if (deleteButton !== undefined) {
-        deleteButton.addEventListener("click", () => {
-            onDeleteButtonClick(deleteButton);
-        });
-    }
-}
-
-let checkBoxLabels = document.getElementsByClassName("task-checkbox-label");
-for (let i = 0; i < checkBoxLabels.length; i++) {
-    const label = checkBoxLabels[i];
-    const checkBox = label.getElementsByTagName("checkbox")[0];
-    if (checkBox !== undefined) {
-        checkBox.addEventListener("change", () => {
-            onCheckBoxChange(checkBox);
-        })
-    }
 }
 
 function onDeleteButtonClick(deleteButton) {
-    deleteButton.parentNode.remove();
+    const li = deleteButton.parentNode;
+    const span = li.getElementsByClassName("task")[0];
+    const taskName = span.innerHTML;
+    const taskIndex = findTaskIndex(taskName);
+    console.log("remove " + taskName + ", index " + taskIndex);
+    if (taskIndex > -1) {
+        tasks.splice(taskIndex, 1);
+    }
+    
+    li.remove();
+    storeTasks(tasks);
 }
 
 function onCheckBoxChange(checkBox) {
     const label = checkBox.parentElement;
-    const task = label.parentElement.getElementsByClassName("task")[0];
-    task.classList.toggle("task-done");
+    const taskElement = label.parentElement.getElementsByClassName("task")[0];
+    taskElement.classList.toggle("task-done");
+    
+    const taskIndex = findTaskIndex(taskElement.value);
+    console.log("Index: " + taskIndex);
+    tasks[taskIndex].checked = !tasks[taskIndex].checked;
+    storeTasks(tasks);
+}
+
+function createTask(taskName) {
+    return {
+        id: taskCount++,
+        task: taskName,
+        checked: false
+    };
+}
+
+function storeTasks(tasks) {
+    const json = JSON.stringify(tasks);
+    console.log("Store: " + json);
+    localStorage.setItem("tasks", json);
+}
+
+function findTaskIndex(taskName) {
+    return tasks.findIndex((item) => {
+        console.log("finding... current: " + item.task);
+        return item.task === taskName;
+    });
 }
